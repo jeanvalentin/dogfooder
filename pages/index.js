e4df944f7f00cc1de4cf57f1bb9bd0491ae9c902
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-05-08 14:35:55 +0200

    Add GitHub link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Box, Button, createStyles, Grid, Overlay, Text, Title } from '@mantine/
 import { useViewportSize } from '@mantine/hooks';
 import { getCookie, setCookies } from 'cookies-next';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 const useStyles = createStyles((theme) => ({
@@ -64,7 +65,7 @@ export default function Index() {
   return <>
     <div className={classes.wrapper} style={{ height: viewportSize.height, width: viewportSize.width, overflow: 'hidden' }}>
       <Overlay color="#000" opacity={0.65} zIndex={1} style={{ height: viewportSize.height, width: viewportSize.width }} />
-      <div className={classes.inner}>
+      <div className={classes.inner} style={{ height: viewportSize.height, width: viewportSize.width }}>
         <Box style={{ height: viewportSize.height / 5 }} />
         <Box style={{ height: viewportSize.height / 5 }}>
           <Title className={classes.title}>
@@ -89,6 +90,15 @@ export default function Index() {
             </Grid.Col>
           </Grid>
         </Box>
+        <div style={{ position: 'absolute', bottom: 8, right: 8 }}>
+          <Link href='https://github.com/jeanvalentin/dogfooder'>
+            <a>
+              <svg height="32" viewBox="0 0 16 16" width="32">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   </>
