e5cdd51eed920822f5c973af796987f7eac7bf6e
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-05-11 22:57:45 +0200

    Add timer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, createStyles, Grid, Overlay, Text, Title } from '@mantine/core';
+import { Box, Button, createStyles, Overlay, Text, Title } from '@mantine/core';
 import { useViewportSize } from '@mantine/hooks';
 import { getCookie, setCookies } from 'cookies-next';
 import dayjs from 'dayjs';
@@ -29,8 +29,12 @@ const useStyles = createStyles((theme) => ({
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
   },
 
-  highlight: {
-    color: theme.colors[theme.primaryColor][4],
+  hungry: {
+    color: theme.colors.red[8],
+  },
+
+  nothungry: {
+    color: theme.colors.green[8],
   },
 }));
 
@@ -44,24 +48,31 @@ const getTimeToken = time => dayjs(time).day() * 2 + (12 <= dayjs(time).hour()) 
 export default function Index() {
   const { classes } = useStyles();
   const { height, width } = useViewportSize();
-  const [timeToken, setTimeToken] = useState(getCookie('timeToken') ?? 0);
-  const [formattedTime, setFormattedTime] = useState('');
+  const [nextMealTime, setNextMealTime] = useState(dayjs(getCookie('nextMealTime') ?? dayjs()));
   const [viewportSize, setViewportSize] = useState({ height: 0, width: 0 });
+  const [dogIsHungry, setDogIsHungry] = useState(false);
 
   const feed = () => {
-    const newTimeToken = getTimeToken(dayjs());
-    setTimeToken(newTimeToken);
-    setCookies('timeToken', newTimeToken, { expires: dayjs().add(30, 'd').toDate() });
+    const now = dayjs();
+    const newNextMealTime = now.set('h', now.hour < 12 ? 11 : 23).set('m', 59).set('s', 59);
+    setNextMealTime(newNextMealTime);
+    setCookies('nextMealTime', newNextMealTime.toJSON(), { expires: dayjs().add(30, 'd').toDate() });
   };
 
   const unfeed = () => {
-    const oldTimeToken = getTimeToken(dayjs().subtract(12, 'h'));
-    setTimeToken(oldTimeToken);
-    setCookies('timeToken', oldTimeToken, { expires: dayjs().add(30, 'd').toDate() });
+    const newNextMealTime = dayjs().subtract(1, 'h');
+    setNextMealTime(newNextMealTime);
+    setCookies('nextMealTime', newNextMealTime.toJSON(), { expires: dayjs().add(30, 'd').toDate() });
   };
 
-  useEffect(() => setFormattedTime(formatTime(timeToken)), [timeToken]);
   useEffect(() => setViewportSize({ width, height }), [height, width]);
+  useEffect(() => {
+    setDogIsHungry(nextMealTime.isBefore(dayjs()));
+    const interval = setInterval(() => {
+      setDogIsHungry(nextMealTime.isBefore(dayjs()));
+    }, 120000);
+    return () => clearInterval(interval);
+  }, [nextMealTime]);
 
   return <>
     <div className={classes.wrapper} style={viewportSize}>
@@ -70,27 +81,21 @@ export default function Index() {
         <Box style={{ height: viewportSize.height / 5 }} />
         <Box style={{ height: viewportSize.height / 5 }}>
           <Title className={classes.title}>
-            Le chien a mangé
-            {' '}
-            <Text component="span" inherit className={classes.highlight}>
-              {formattedTime}
+            <Text component="span" inherit className={dogIsHungry ? classes.hungry : classes.nothungry}>
+              {dogIsHungry ? `Le chien n'a pas mangé !` : `Le chien a mangé ${formatTime(getTimeToken(nextMealTime))}.`}
             </Text>
           </Title>
         </Box>
         <Box style={{ height: viewportSize.height / 5 }} />
         <Box style={{ margin: 'auto', textAlign: 'center' }}>
-          <Grid gutter="xl" px="xl">
-            <Grid.Col span={6}>
-              <Button variant="white" size="xl" onClick={feed} style={{ width: '100%' }}>
-                <Text size="xl">Nourrir le chien</Text>
-              </Button>
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <Button variant="outline" size="xl" onClick={unfeed} style={{ width: '100%' }}>
-                <Text size="xl">En fait non</Text>
-              </Button>
-            </Grid.Col>
-          </Grid>
+          {dogIsHungry ?
+            <Button variant="white" size="xl" onClick={feed} px={100}>
+              <Text size="xl">Nourrir le chien</Text>
+            </Button>
+            :
+            <Button variant="outline" size="xl" onClick={unfeed} px={100}>
+              <Text size="xl">En fait non</Text>
+            </Button>}
         </Box>
         <div style={{ position: 'absolute', bottom: 8, right: 8 }}>
           <Link href="https://github.com/jeanvalentin/dogfooder">
